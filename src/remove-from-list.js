0a888496428dfeb55b134fa5294177f8cb139a58
Kirill Kovalev <kirill.kovalev1502@mail.ru>
2022-05-01 20:01:12 +0300

    feat: 2 tasks ready

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
+function removeKFromList(l, k) {
+
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  // if(l.value === k){
+  //   l = l.next;
+  // }
+
+  // while(l.next){
+  //   if(l.next.value === k){
+  //     l.next = l.next.next;
+  //   }
+  // }
+
+  // return l;
 }
 
 module.exports = {
